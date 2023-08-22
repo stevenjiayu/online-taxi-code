ce71fbececed8dfb6153ea6f1cba3f364561dc91
stevenwu <stevenjiayu1229@gmail.ocm>
2023-08-22 16:58:18 +0800

    jwt 生成2

--- internal-common/src/main/java/com/xcode/util/Jwtutils.java
+++ internal-common/src/main/java/com/xcode/util/Jwtutils.java
@@ -3,6 +3,8 @@ package com.xcode.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -13,10 +15,12 @@ public class Jwtutils {
 
 
     private static final  String   SIGN = "CPFxcode!@#$%$";
+    private static final  String   JWT_KEY = "passengerPhone";
 
-    public static String generatorToken(Map<String,String> map){
+    public static String generatorToken(String passengerPhone){
 
-
+        Map<String,String> map =new HashMap<>();
+        map.put(JWT_KEY ,passengerPhone);
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE,1);
         Date date = calendar.getTime();
@@ -38,6 +42,13 @@ public class Jwtutils {
         return sign;
     }
 
+    public static  String parseToken(String token){
+
+        DecodedJWT verify= JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim claim = verify.getClaim(JWT_KEY );
+        return claim.toString();
+    }
+
 
     public static void main(String[] args) {
 
@@ -49,13 +60,15 @@ public class Jwtutils {
         System.out.println(calendar);
          */
 
-        Map<String,String> map =new HashMap<>();
-        map.put("name","alex");
-        map.put("age","18");
+//        Map<String,String> map =new HashMap<>();
+//        map.put("name","alex");
+//        map.put("age","18");
 
-        String s =generatorToken(map);
+
+        String s =generatorToken("13922323");
 
         System.out.println(s);
+        System.out.println(parseToken(s));
 
 //        map.forEach((k,v)->
 //
